682cfc35304d2f2e2b81000af48f3a85ecc9c290
dodamtanguri <sh663746@gmail.com>
2020-12-03 18:15:56 +0900

    N15727

--- algorithm/src/N15727.java
+++ algorithm/src/N15727.java
@@ -1,5 +1,14 @@
-public class N15727 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+
+public class N15727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int distance = Integer.parseInt(bf.readLine());
+        int answer = (int)Math.ceil((double)distance/5);
+
+        System.out.println(answer);
     }
 }
