80cc4d038449498a94ed4c89947db2b716568d1d
dodamtanguri <sh663746@gmail.com>
2020-12-07 18:16:56 +0900

    N9095_DP

--- DynamicProgramming/src/N9095.java
+++ DynamicProgramming/src/N9095.java
@@ -1,5 +1,23 @@
-public class N9095 {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Scanner;
 
+
+public class N9095 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[] d = new int[11];
+        d[0] = 1;
+        d[1] = 1;
+        d[2] = 2;
+
+        for (int i = 3; i < d.length; i++) {
+            d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            System.out.println(d[n]);
+        }
     }
 }
