63ea249fb6076c23d9c0083da371f28067d3db55
dodamtanguri <sh663746@gmail.com>
2021-05-26 00:17:37 +0900

    태스트케이스 14번 실패

--- Programmers/Level1/src/로또의최고순위와최저순위.java
+++ Programmers/Level1/src/로또의최고순위와최저순위.java
@@ -1,33 +1,39 @@
 import java.util.Arrays;
 
 public class 로또의최고순위와최저순위 {
+    public static void main(String[] args) {
+        로또의최고순위와최저순위 test = new 로또의최고순위와최저순위();
+        int[] lottos = {38, 19, 20, 40, 15, 25};
+        int[] win_nums = {39, 20, 21, 41, 16, 26};
+        System.out.println(Arrays.toString(test.solution(lottos, win_nums)));
+    }
+
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        int[] answerTemp = {6,5,4,3,2,1};
+        int[] answerTemp = {6, 5, 4, 3, 2, 1};
         int winScore = 0;
         int unkownNum = 0;
         for (int i = 0; i < lottos.length; i++) {
+            if (lottos[i] == 0) {
+                unkownNum++;
+            }
             for (int j = 0; j < lottos.length; j++) {
-                if (lottos[j] == 0) { unkownNum++; }
-                if (win_nums[i] == lottos[j]) { winScore ++; }
+                if (win_nums[i] == lottos[j]) {
+                    winScore++;
+                }
             }
         }
-       for (int i = 0; i < answerTemp.length; i++) {
-           if (answerTemp[i] == winScore) {
-               answer[0] = i;
-           }
-           if (answerTemp[i] == (winScore+unkownNum)) {
-               answer[1] = i;
-           }
-       }
+        for (int i = 0; i < answerTemp.length; i++) {
+             if (answerTemp[i] == (winScore + unkownNum)) {
+                answer[0] = i + 1;
+            }
+            if (winScore == 0) {
+                answer[1] = 6;
+            } else if (answerTemp[i] == winScore) {
+                answer[1] = i + 1;
+            }
+        }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        로또의최고순위와최저순위 test = new 로또의최고순위와최저순위();
-        int[] lottos = {44, 1, 0, 0, 31, 25};
-        int[] win_nums = {31, 10, 45, 1, 6, 19};
-        System.out.println(Arrays.toString(test.solution(lottos, win_nums)));
     }
 }
